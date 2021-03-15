3b70473a9ab729634ef4e8892417ee4747dcfc6e
Tutarat <Tutarat@yandex.ru>
2021-03-15 17:51:48 +0300

    rename test1

--- src/main/java/Tests/SecondTest.java
+++ src/main/java/Tests/SecondTest.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Test;
 
 public class SecondTest extends TestBase {
     @Test
-    public void FirstTest(){
+    public void FirstTest1(){
         driver.get(SITE_URL);
 
 
     }
 
     @Test
-    public void SecondTest(){
+    public void SecondTest1(){
 
 
     }
